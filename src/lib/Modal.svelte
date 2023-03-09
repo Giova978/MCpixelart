448f98c92dc3ea707ce8410c2c064f60d3f0af4f
Giova <giovanni.hawas@gmail.com>
2023-03-09 17:44:58 -0300

    Generate command feature
    Added the generate command feature and its corresponding interface inside the modal
    
    Added 'clickOtuside' helper function for closing the modal
    
    Added textureNmaeToId for generate command feature

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,12 +1,39 @@
 <script lang="ts">
     import blockPositions from "$lib/data/blockPositions.json";
     import { createEventDispatcher, tick } from "svelte";
+    import {} from "svelte/animate";
+    import { clickOutside } from "./clickOutside";
+    import {
+        AlongAxis,
+        frontFacingEast,
+        frontFacingNorth,
+        frontFacingSouth,
+        frontFacingWest,
+        generateSetblock,
+        type Facing,
+    } from "./generateSetblock";
 
     export let show = false;
     export let spriteSheet: HTMLImageElement;
     export let newImageData: string[][];
     let canvas: HTMLCanvasElement;
+    let textarea: HTMLTextAreaElement;
     const dispatch = createEventDispatcher();
+
+    let showCanvas = true;
+    let commands = "";
+    let showCommand = false;
+    let opposingAxisOffset = 10;
+    let axis = AlongAxis.AlongX;
+    let facing: Facing = frontFacingNorth;
+
+    $: {
+        if (axis == AlongAxis.AlongZ) {
+            facing = frontFacingWest;
+        } else {
+            facing = frontFacingNorth;
+        }
+    }
 
     function render(ctx: CanvasRenderingContext2D, blocksMatrix: string[][]) {
         for (let yFactor = 0; yFactor < blocksMatrix.length; yFactor++) {
@@ -21,7 +48,19 @@
         }
     }
 
-    $: if (show) {
+    function generateCommands() {
+        commands = generateSetblock(newImageData, axis, facing, opposingAxisOffset);
+
+        showCommand = true;
+        showCanvas = false;
+    }
+
+    function swapCanvas() {
+        showCommand = false;
+        showCanvas = true;
+    }
+
+    $: if (showCanvas && show) {
         // Wait for the html to render before updating the canvas
         tick().then(() => {
             render(canvas.getContext("2d")!, newImageData);
@@ -31,16 +70,84 @@
 
 {#if show}
     <article class="modal">
-        <div>
+        <div use:clickOutside on:clickOutside={() => dispatch("hide")}>
+            <article>
+                <button on:click={generateCommands}>Generate command</button>
+                <fieldset>
+                    <h2>Axis:</h2>
+                    <label>
+                        <input type="radio" bind:group={axis} name="axis" value={AlongAxis.AlongX} />
+                        Along X
+                    </label>
+
+                    <label>
+                        <input type="radio" bind:group={axis} name="axis" value={AlongAxis.AlongZ} />
+                        Along Z
+                    </label>
+                </fieldset>
+                <fieldset>
+                    <h2>Facing:</h2>
+
+                    {#if axis == AlongAxis.AlongX}
+                        <label>
+                            <input type="radio" bind:group={facing} name="facing" value={frontFacingNorth} />
+                            Facing North
+                        </label>
+                        <label>
+                            <input type="radio" bind:group={facing} name="facing" value={frontFacingSouth} />
+                            Facing South
+                        </label>
+                    {:else}
+                        <label>
+                            <input type="radio" bind:group={facing} name="facing" value={frontFacingWest} checked />
+                            Facing West
+                        </label>
+                        <label>
+                            <input type="radio" bind:group={facing} name="facing" value={frontFacingEast} />
+                            Facing East
+                        </label>
+                    {/if}
+                </fieldset>
+                <fieldset>
+                    <label>
+                        Distance from the player in the opposite direction
+                        <input type="number" value={opposingAxisOffset} />
+                    </label>
+                </fieldset>
+            </article>
             <div>
-                <canvas bind:this={canvas} width={newImageData[0].length * 16} height={newImageData.length * 16} />
+                {#if showCommand}
+                    <textarea autocomplete="off" readonly bind:this={textarea}>{commands}</textarea>
+                {:else}
+                    <canvas bind:this={canvas} width={newImageData[0].length * 16} height={newImageData.length * 16} />
+                {/if}
             </div>
-            <button on:click={() => dispatch("hide")}>X</button>
+            {#if showCommand}
+                <button on:click={() => navigator.clipboard.writeText(textarea.value)} class="copy-to-clipboard"
+                    >Copy to Clipboard</button
+                >
+                <button on:click={swapCanvas} id="noclose" class="back">Back</button>
+            {/if}
+            <button on:click={() => dispatch("hide")} class="close">X</button>
         </div>
     </article>
 {/if}
 
 <style lang="scss">
+    @function color($varName, $A: 1) {
+        @return Rgba(#{var($varName)}, $A);
+    }
+
+    button {
+        font-family: "Rubik", sans-serif;
+        cursor: pointer;
+        border: none;
+        border-radius: 5px;
+
+        color: color(--text-color);
+        background: color(--accent-color);
+    }
+
     .modal {
         width: 100%;
         height: 100%;
@@ -53,14 +160,45 @@
         align-items: center;
         background: rgba(#000, 0.4);
 
+        article {
+            max-width: 145px;
+
+            button {
+                display: block;
+                padding: 0.5rem;
+                font-size: 1.3rem;
+            }
+
+            fieldset {
+                color: #fff;
+                margin: 0.5rem 0;
+                padding: 0.5rem;
+                border-radius: 5px;
+                background-color: color(--accent-color-2);
+
+                label {
+                    margin-top: 0.2rem;
+                    display: block;
+                }
+
+                input[type="number"] {
+                    max-width: 50px;
+                }
+            }
+        }
+
         & > div {
             width: auto;
             position: relative;
-            background: #fff;
+            background: none;
+            display: grid;
+            grid-template-columns: 10% minmax(1fr, 80vw);
+            gap: 1rem;
 
             div {
+                grid-column: 2;
                 max-width: 90vw;
-                max-height: 90vh;
+                max-height: 80vh;
                 overflow-y: scroll;
                 overflow-x: auto;
                 scrollbar-gutter: stable;
@@ -68,10 +206,17 @@
                 canvas {
                     display: block;
                 }
+
+                textarea {
+                    width: 75vw;
+                    resize: none;
+                    height: 78vh;
+                    outline: none;
+                    border: none;
+                }
             }
 
-            button {
-                font-family: "Rubik", sans-serif;
+            .close {
                 position: absolute;
                 width: 30px;
                 height: 30px;
@@ -83,6 +228,33 @@
 
                 border: 0;
                 border-radius: 5px;
+            }
+
+            .back {
+                @extend .close;
+                width: auto;
+                left: 0;
+                background-color: color(--accent-color-2);
+            }
+
+            .copy-to-clipboard {
+                @extend .close;
+                width: auto;
+                right: 0;
+
+                &:active {
+                    animation: scaleDown 0.8s ease forwards;
+                }
+            }
+
+            @keyframes scaleDown {
+                from {
+                    transform: scale(1);
+                }
+
+                to {
+                    transform: scale(0.8);
+                }
             }
         }
     }
---
+++ src/lib/generateSetblock.ts
@@ -0,0 +1,104 @@
+import textureToId from "$lib/data/textureNameToId.json"
+
+export enum AlongAxis {
+	AlongX,
+	AlongZ,
+}
+
+export interface Facing {
+	Front: string;
+	Back: string;
+	Side1: string;
+	Side2: string;
+}
+
+export enum frontFacingEast {
+	Front = "east",
+	Back = "west",
+	Side1 = "south",
+	Side2 = "north",
+}
+
+export enum frontFacingWest {
+	Front = "west",
+	Back = "east",
+	Side1 = "south",
+	Side2 = "north"
+}
+
+export enum frontFacingSouth {
+	Front = "south",
+	Back = "north",
+	Side1 = "east",
+	Side2 = "west"
+}
+
+export enum frontFacingNorth {
+	Front = "north",
+	Back = "south",
+	Side1 = "east",
+	Side2 = "north"
+}
+export function generateSetblock(blocksMatrix: string[][], axis: AlongAxis, facing: Facing, oposingAxisOffset: number) {
+	if (blocksMatrix.length < 1) return "";
+	const generateCommand = getCommandGenerator(axis)
+	let comamnds: string = ""
+
+	const imageTop = blocksMatrix.length - 1
+	let imageWidth = 0
+
+	if (facing.Front == "north" || facing.Front == "east") imageWidth = blocksMatrix[0].length - 1
+
+	for (let y = 0; y < blocksMatrix.length; y++) {
+		for (let x = 0; x < blocksMatrix[y].length; x++) {
+			const [blockId, ...states] = (textureToId as { [key: string]: string })[blocksMatrix[y][x]].split("~")
+			let extraData = '['
+
+			if (states.length > 0) {
+				states.map((state: string) => {
+					switch (state) {
+						case 'front':
+							extraData += `facing=${facing.Front}`
+							break
+						case 'back':
+							extraData += `facing=${facing.Back}`
+							break
+						case 'side':
+							extraData += `facing=${facing.Side1}`
+							break
+						case 'sideup':
+							extraData += `facing=up`
+						case 'axisy':
+							extraData += `axis=y`
+						default:
+							console.log(blockId, state)
+					}
+				})
+			}
+
+			if (blockId.includes("trapdoor")) {
+				extraData += `facing=${facing.Back},open=true`
+			}
+
+			extraData += ']'
+			// We substract from the top coordinates because the image data comes from the top-left to bottom-right
+			// If the direction the image need to face is north or east we need to mirror x for it to be correctly oriented, we do that by substracting the width of the image from the current x value, this way we start from the right instead of the left
+			comamnds += "\n" + generateCommand(mirrorX(x, imageWidth), imageTop - y, oposingAxisOffset, blockId, extraData)
+		}
+	}
+	return comamnds.trim()
+}
+
+function mirrorX(x: number, imageWidth: number) {
+	return imageWidth > 0 ? imageWidth - x : x
+}
+
+function getCommandGenerator(axis: AlongAxis) {
+	const baseCommand = "execute as @s run setblock"
+	// x will either be the Z coordinate or X coordinate depending on the axis, is called x because we are producing a 2D canvas that will get generate on a 3D space
+	if (axis == AlongAxis.AlongX) {
+		return (x: number, y: number, opposingAxisOffset: number, blockId: string, extraData: string) => `${baseCommand} ~${x} ~${y} ~${opposingAxisOffset} minecraft:${blockId}${extraData}`
+	} else {
+		return (x: number, y: number, opposingAxisOffset: number, blockId: string, extraData: string) => `${baseCommand} ~${opposingAxisOffset} ~${y} ~${x} minecraft:${blockId}${extraData}`
+	}
+}
